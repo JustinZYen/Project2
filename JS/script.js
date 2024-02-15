fe0b36c3eb004118112a7acd88a5749243dde9b6
Justin Yen <justzyen@gmail.com>
2024-02-15 13:29:54 -0700

    tracking onmousedown

--- JS/script.js
+++ JS/script.js
@@ -8,14 +8,21 @@ function position(event) {
 
 const movement = (event) => {
     //ctx.fillRect(event.clientX, event.clientY, 100, 100);
-    ctx.lineTo(event.clientX,event.clientY);
-    ctx.stroke();
+    if (mouseDown) {
+        ctx.lineTo(event.clientX,event.clientY);
+        ctx.stroke();
+    }
 }
 
 const canvas = document.getElementById("testCanvas");
 const ctx = canvas.getContext("2d");
+let mouseDown = false;
+
 ctx.fillStyle = "green";
 //ctx.fillRect(10, 10, 100, 100);
 ctx.beginPath();
 ctx.moveTo(0,0);
-canvas.addEventListener('click', movement, false);
+//canvas.addEventListener('click', movement, false);
+canvas.addEventListener('mousemove',movement,false);
+document.body.onmousedown = () => mouseDown = true;
+document.body.onmouseup = () => mouseDown = false;
