9ee6b2b5116240d6eaf75a862b16d53d005515a9
Justin Yen <justzyen@gmail.com>
2024-02-15 11:56:01 -0700

    Drawing lines

--- JS/script.js
+++ JS/script.js
@@ -7,13 +7,15 @@ function position(event) {
 }
 
 const movement = (event) => {
-    //const canvas = document.getElementById("myCanvas");
-    //const ctx = canvas.getContext("2d");
-    ctx.fillRect(event.clientX, event.clientY, 100, 100);
+    //ctx.fillRect(event.clientX, event.clientY, 100, 100);
+    ctx.lineTo(event.clientX,event.clientY);
+    ctx.stroke();
 }
 
 const canvas = document.getElementById("testCanvas");
 const ctx = canvas.getContext("2d");
 ctx.fillStyle = "green";
-ctx.fillRect(10, 10, 100, 100);
+//ctx.fillRect(10, 10, 100, 100);
+ctx.beginPath();
+ctx.moveTo(0,0);
 canvas.addEventListener('click', movement, false);
