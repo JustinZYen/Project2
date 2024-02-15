d1b824919b59c808ae1db69d2396040be5c8d5e2
Justin Yen <justzyen@gmail.com>
2024-02-14 21:56:58 -0700

    Today I learned about the defer attribute

--- JS/script.js
+++ JS/script.js
@@ -2,6 +2,20 @@ function func() {
     alert("Hello!");
 }
 
-function position() {
-    alert("mouse move");
+function position(event) {
+    alert(event.clientX);
 }
+
+const movement = (event) => {
+    //const canvas = document.getElementById("myCanvas");
+    //const ctx = canvas.getContext("2d");
+   
+    alert(event.clientX);
+}
+
+const canvas = document.getElementById("testCanvas");
+alert(canvas);
+const ctx = canvas.getContext("2d");
+ctx.fillStyle = "green";
+ctx.fillRect(10, 10, 100, 100);
+canvas.addEventListener('click', movement, false);
